2d7351dc57c055e55e1c06cc2a53725a134c954b
kelt-uva <kelt.paehlig@student.uva.nl>
2023-09-04 14:46:03 +0200

    spelling fixes

--- challenge_day1/Sebastian.py
+++ challenge_day1/Sebastian.py
@@ -1,4 +1,4 @@
-from Xiao import xiao_charachter
+from Xiao import xiao_character
 from ruben import ruben_character
 from sofia import sofia_character
 from Kelt import kelt_character
@@ -9,7 +9,7 @@ def Sebastian():
 sebastian_character = "Luna"
 
 sebastian_paragraphs = [
-    f"{sebastian_charcter} felt a shiver run down her spine, seeing the map. Beside her, {xiao_charachter}, {kelt_character}, {ruben_character}, ad {sofia_character} pondered its origins.",
-    f"Under the moonlight, {sebastian_character} played a tune on her flute, mesmerizing {xiao_charachter}, {kelt_character}, {ruben_character}, ad {sofia_character}.",
-    f"With the dawn's first light, {sebastian_character} guided {xiao_charachter}, {kelt_character}, {ruben_character}, ad {sofia_character} through the dense forest."
+    f"{sebastian_character} felt a shiver run down her spine, seeing the map. Beside her, {xiao_character}, {kelt_character}, {ruben_character}, and {sofia_character} pondered its origins.",
+    f"Under the moonlight, {sebastian_character} played a tune on her flute, mesmerizing {xiao_character}, {kelt_character}, {ruben_character}, and {sofia_character}.",
+    f"With the dawn's first light, {sebastian_character} guided {xiao_character}, {kelt_character}, {ruben_character}, and {sofia_character} through the dense forest."
 ]
