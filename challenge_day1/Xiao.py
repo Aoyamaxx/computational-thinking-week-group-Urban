e1631e5f77fdfb084f243ce1d9fd40fe5cef8c53
Quanpu Xiao <aoyamaxx@re-byods-146-50-199-238.wireless.uva.nl>
2023-09-04 14:46:46 +0200

    uhm

--- challenge_day1/Xiao.py
+++ challenge_day1/Xiao.py
@@ -1,7 +1,7 @@
 from Sebastian import sebastian_character
-from ruben import Rruben_character
-from sofia import sofia_character
-from Kelt import kelt_character
+from ruben import Ruben, ruben_character
+from sofia import Sofia, sofia_character
+from Kelt import Kelt, kelt_character
 
 def Xiao():
     return "Xiao"
