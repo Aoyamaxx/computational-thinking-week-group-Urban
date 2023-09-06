d238255b2f0238510674c7f9fc6fa7d39a774799
sebastian-graeff <sebgraeff@hotmail.com>
2023-09-06 13:04:28 +0200

    abc

--- challenge_day3/tests/station1.py
+++ challenge_day3/tests/station1.py
@@ -1,3 +1,6 @@
+import pandas as pd
 
-def solution_station_1():
-    return
+def solution_station_1(position):
+    df = pd.read_excel("FibonacciSequence.xlsx")
+    team_nr = df["Value"][df["Position"] == position]
+    return team_nr
--- challenge_day3/tests/station5.py
+++ challenge_day3/tests/station5.py
@@ -1,3 +1,6 @@
+import pandas as pd
 
-def solution_station_5():
-    return
+def solution_station_5(name):
+    df = pd.read_excel("NameGroup.xlsx")
+    team_nr = df["Team"][df["Name"].isin([name])]
+    return team_nr
